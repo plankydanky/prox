48567826e7f751c060794abbb96c432479a9e2f8
plankydanky <75154977+plankydanky@users.noreply.github.com>
2021-09-01 00:41:41 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
-import useProxy from 'rocket-booster';
+import RocketBooster from 'rocket-booster';
 
 addEventListener('fetch', (event) => {
   const proxy = useProxy();
   proxy.use('/', {
-    upstream: {
-      domain:  'unifi.kingcunt.dev',
-      protocol: 'https',
-    },
-  });
+  upstream: {
+    domain: 'unifi.kingcunt.dev',
+    protocol: 'https',
+  },
 
-  const response = proxy.apply(event.request);
+  optimization: {
+    mirage: true,
+    minify: {
+      javascript: true,
+      css: true,
+      html: true,
+    },
+  },
+
+  firewall: [
+    {
+      field: 'country',
+      operator: 'in',
+      value: ['CN', 'KP', 'SY', 'PK', 'CU'],
+    },
+  ],
+
+   cors: {
+   origin: '*',
+  },
+};
+
+addEventListener('fetch', (event) => {
+  const booster = new RocketBooster(config);
+  const response = booster.apply(event.request);
   event.respondWith(response);
 });
